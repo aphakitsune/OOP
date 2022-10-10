27fee041247d6dbb45a8c219613f9272af297f14
J-Reacher <113431491+J-Reacher@users.noreply.github.com>
2022-10-10 03:11:22 +0000

    Create main.cpp

---
+++ QL_Sinhvien/main.cpp
@@ -0,0 +1,26 @@
+// struct.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+// compile files
+// /bin/bash
+// $ g++ -c functions.cpp main.cpp
+// $ g++ -o main.exe main.o functions.o
+
+
+#include <iostream>
+#include <cstring>
+#include "functions.h"
+
+
+using namespace std;
+
+
+int main(){
+	SinhVien * ds;
+	int n;
+	cout << "Cho biet so luong sinh vien : ";cin >> n;
+	cin.ignore(); // xoa vung dem ban phim
+	ds = new SinhVien[n];
+	nhapsv(ds, n);
+	xuatsv(ds, n);
+	  
+}
