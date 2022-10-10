91722022462ec1c0fe734582bf369e84dec8cbe4
J-Reacher <113431491+J-Reacher@users.noreply.github.com>
2022-10-10 04:10:17 +0000

    Update functions.h

--- QL_Sinhvien/functions.h
+++ QL_Sinhvien/functions.h
@@ -2,11 +2,15 @@
 #include <cstdio>
 
 typedef struct {
-	char * masv;
+	char* masv;
 	char* hoten;
 	char* quequan;
 } SinhVien;
 
+
 void removeNewline(char* x[]);
+
 void nhapsv(SinhVien sv[], int n);
 void xuatsv(SinhVien sv[], int n);
+void timkiem(SinhVien sv[], int n);
+void xoasv(SinhVien sv[], int &n);
